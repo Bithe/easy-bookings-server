968fab22321d7b078d04d74f46a7552a503f795a
Bithe <farzana.hossain5@gmail.com>
2024-08-12 19:55:02 +0600

    server

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+
+//middleware
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req,res)=>{
+    res.send('easy booking server running');
+})
+
+
+app.listen(port, ()=>{
+    console.log(`easy booking sever running on port ${port}`);
+})
