a784ac9b7b82b011d2caddfcbddde26e91e470db
Bithe <farzana.hossain5@gmail.com>
2024-09-07 16:38:47 +0600

    get a single room data

--- index.js
+++ index.js
@@ -14,6 +14,20 @@ const port = process.env.PORT || 5000;
 
 // MIDDLEWARE ------------------------
 
+const corsConfig = {
+  origin:[
+    "*",
+    "http://localhost:5174",
+    "http://localhost:5175",
+    "http://localhost:5173"
+  ],
+  credentials: true,
+};
+
+app.use(cors(corsConfig));
+app.use(express.json());
+
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mfawfp8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,27 +42,39 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     
     const roomsCollection = client.db("easyBookingDb").collection("roomsCollection");
 
 
     // GET ALL THE ROOMS DATA
-    app.get('/rooms', async(res, req)=>{
-      const rooms = await roomsCollection.find().toArray();
-      res.send(rooms);
+     app.get('/rooms', async(req, res) => {
+      const result = await roomsCollection.find().toArray();
+      res.send(result);
+     })
+
+    //  GET A SINGLE ROOM DATA FOR ROOM DETAILS PAGE
+    app.get('/room/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await roomsCollection.findOne(query);
+      res.send(result);
+
     })
+
+
+     
 
 
 
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
